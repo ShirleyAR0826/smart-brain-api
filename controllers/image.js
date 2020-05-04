2d1bce9075cf0e6ab9598b13bc7dd306aa10d3fb
Shirley <shirley.apellido@gmail.com>
2020-05-04 17:10:40 +0800

    commiting api

---
+++ controllers/image.js
@@ -0,0 +1,28 @@
+const clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '776b4d44144849f5ae14e9faa64e6541'
+});
+
+const handleApiCall = (req, res) => {
+    app.models.predict(clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => res.json(data))
+    .catch(err => res.status(400).json('Unable to work with API'));
+}
+
+
+const handleImage = (req, res, db) => {
+    const {id} = req.body;
+    db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+        res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json('unable to get entries'))
+}
+
+module.exports = {
+    handleImage: handleImage,
+    handleApiCall: handleApiCall
+}
